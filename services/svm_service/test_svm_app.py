fab20193c7ab1d0b703eafcecf7157eb69d12019
hadily <hadilyahiaoui02@gmail.com>
2024-12-12 10:26:27 +0100

    Added test svm

---
+++ services/svm_service/test_svm_app.py
@@ -0,0 +1,43 @@
+import pytest
+import io
+from io import BytesIO
+import sys
+import os
+import warnings
+
+# Ignore all warnings
+warnings.filterwarnings("ignore")
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from svm_app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_predict_valid_file(client):
+    # Path to the test audio file
+    audio_file_path = os.path.join(os.path.dirname(__file__), 'test_files', 'disco.00004.wav')
+
+    # Open the audio file and send it in the POST request
+    with open(audio_file_path, 'rb') as audio_file:
+        data = {
+            'file': (BytesIO(audio_file.read()), 'disco.00004.wav')
+        }
+
+        response = client.post('/predict', data=data, content_type='multipart/form-data')
+
+        # Check if the response is valid
+        assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+        assert b"genre" in response.data, "Response does not contain 'genre'"
+
+def test_predict_no_file_provided(client):
+    # Send a POST request without any file
+    response = client.post('/predict', data={}, content_type='multipart/form-data')
+    
+    # Assert that the status code is 400
+    assert response.status_code == 400, f"Unexpected status code: {response.status_code}"
+    
+    # Assert that the response contains the expected error message
+    assert b"No file provided" in response.data, "Error message for missing file is incorrect or not present"
